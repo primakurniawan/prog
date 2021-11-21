c4ef50a6c4d355a206832e1c896796a39b9cbde1
Prima Tondy Arya Kurniawan <primakurniawan666@gmail.com>
2021-11-21 14:35:16 +0700

    orm

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module prog
 go 1.17
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
 	github.com/labstack/echo/v4 v4.6.1 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
@@ -13,4 +16,6 @@ require (
 	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gorm.io/driver/mysql v1.2.0 // indirect
+	gorm.io/gorm v1.22.3 // indirect
 )
--- main.go
+++ main.go
@@ -4,28 +4,54 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type User struct {
+	ID       int
 	Email    string `json:"email" form:"email"`
 	Fullname string `json:"fullname" form:"fullname"`
 }
 
+var db *gorm.DB
+
 func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&User{})
 	e := echo.New()
 
-	e.POST("/users", getUserById)
+	e.GET("/users", getAllUsers)
+	e.POST("/users", createUser)
 
 	e.Start(":8000")
 }
 
-func getUserById(c echo.Context) error {
-	user := User{}
+func getAllUsers(c echo.Context) error {
+	users := []User{}
 
-	c.Bind(&user)
+	db.Find(&users)
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status": "success",
+		"users":  users,
+	})
+}
+
+func createUser(c echo.Context) error {
+
+	user := User{}
+	c.Bind(&user)
+	db.Create(&user)
+
+	return c.JSON(http.StatusAccepted, map[string]interface{}{
+		"status": "success",
 		"user":   user,
 	})
+
 }
